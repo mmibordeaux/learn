46e7d1fc19a48570e6bd047eac55652cf57c18c2
Arnaud Levy <arnaud@semiodesign.com>
2014-09-21 16:35:11 +0200

    pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['HTTP_USER'] && password == ENV['HTTP_PASS']
+    end if Rails.env.production?
+  end
 end
