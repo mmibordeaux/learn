4459f21dcf3be1606e47d68e969a08a198e7e992
Arnaud Levy <arnaud@semiodesign.com>
2014-09-21 22:41:51 +0200

    bootstrapped working with codeschool

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.0.4'
-gem 'bootstrap-sass'
-gem 'autoprefixer-rails'
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'bootswatch-rails'
 gem 'sass-rails', '~> 4.0.2'
+gem 'autoprefixer-rails'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,29 @@
 class Student < ActiveRecord::Base
 	belongs_to :promotion
+
+	def to_s
+		"#{firstname} #{lastname}"
+	end
+
+	def codeschool_badges
+		codeschool_results['badges']
+	end
+
+	def codeschool_score
+		codeschool_results['user']['total_score']
+	end
+
+	def codeschool_results
+		require 'open-uri'
+		require 'json'
+		@codeschool_results ||= JSON.parse(open(codeschool_json_url).read)
+	end
+
+	def codeschool_url
+		"https://www.codeschool.com/users/#{codeschool}"
+	end
+
+	def codeschool_json_url
+		"#{codeschool_url}.json"
+	end
 end
