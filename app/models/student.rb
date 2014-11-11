ed46d9aae6b69d4a0cca8555efb14b8058f58cdd
Arnaud Levy <arnaud@semiodesign.com>
2014-11-11 14:36:55 +0100

    codecademy

---
+++ app/models/student.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id              :integer          not null, primary key
+#  firstname       :string(255)
+#  lastname        :string(255)
+#  codeschool      :string(255)
+#  codecademy      :string(255)
+#  promotion_id    :integer
+#  created_at      :datetime
+#  updated_at      :datetime
+#  codeschool_data :text
+#
+
+class Student < ActiveRecord::Base
+  belongs_to :promotion
+    default_scope { order('lastname, firstname') }
+
+    include Codeschool
+    include Codecademy
+
+    def sync!
+      codecademy_sync!
+      codeschool_sync!
+    end
+
+  def to_s
+    "#{firstname} #{lastname}"
+  end
+end
