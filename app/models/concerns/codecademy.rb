7a7d05822779979df795ba6a0697dfa49a7ffd1a
Arnaud Levy <arnaud@semiodesign.com>
2014-11-11 20:02:13 +0100

    skills and badges

--- app/models/concerns/codecademy.rb
+++ app/models/concerns/codecademy.rb
@@ -1,25 +1,33 @@
 module Codecademy
   extend ActiveSupport::Concern
 
-  def codecademy_badges
-    codecademy_results['badges'] if codecademy_results?
-  end
-
   def codecademy_score
-    doc = Nokogiri::HTML(self.codecademy_data) 
-    doc.css("h3.padding-right--quarter")[0].text
+    codecademy_html.css('h3.padding-right--quarter')[0].text unless codecademy_data.nil?
   end
 
-  def codecademy_results?
-    not codecademy_data.nil?
+  def codecademy_skills
+    codecademy_html.css('#completed h5') unless codecademy_data.nil?
+  end
+
+  # TODO rename the db field badges_data
+  def codecademy_badges_list
+    codecademy_badges_html.css('.achievements h5') unless codecademy_data.nil?
   end
 
   def codecademy_url
     "http://www.codecademy.com/fr/#{codecademy}"
   end
 
+  def codecademy_html
+    Nokogiri::HTML(codecademy_data) 
+  end
+
   def codecademy_badges_url
     "http://www.codecademy.com/fr/users/#{codecademy}/achievements"
+  end
+
+  def codecademy_badges_html
+    Nokogiri::HTML(self.codecademy_badges) # TODO codecademy_badges_data
   end
 
   def codecademy_sync!
