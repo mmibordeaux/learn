bccdc240231c0e7d6a675fb890db2feb62ca7f10
Patricia Roche <patriochka@gmail.com>
2016-08-21 12:56:53 +0200

    #2 added lastname firstname in signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_student!
   before_filter :authenticate
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :firstname
+    devise_parameter_sanitizer.for(:sign_up) << :lastname
+  end
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
