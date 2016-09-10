496a93fe590c09f236e56424e37efd1418c17b4e
Arnaud Levy <arnaud@semiodesign.com>
2016-09-10 15:31:59 +0200

    unlock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_student!
-  before_filter :authenticate
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   respond_to :html
@@ -13,12 +12,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :firstname
     devise_parameter_sanitizer.for(:sign_up) << :lastname
     devise_parameter_sanitizer.for(:sign_up) << :promotion_id
-  end
-
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['HTTP_USER'] && password == ENV['HTTP_PASS']
-    end if Rails.env.production?
   end
 
   def admin_only
