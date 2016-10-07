7be81e6aac20237a2a8b9a787a970d43f7e40617
Arnaud Levy <arnaud@semiodesign.com>
2016-10-07 09:30:29 +0200

    fix event teachers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -41,6 +41,6 @@ class EventsController < ApplicationController
     end
 
     def event_params
-      params.require(:event).permit(:name, :date, :description, promotion_ids: [], student_ids: [])
+      params.require(:event).permit(:name, :date, :description, promotion_ids: [], teacher_ids: [])
     end
 end
