96562bbea5cd7bc609f13c07ba7887976af649b1
Arnaud Levy <alevy@lespoupees.paris>
2019-03-05 12:42:38 +0100

    api

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,26 +1,17 @@
 class Api::ProjectsController < Api::ApplicationController
   def show
-    @courses = Course.where(teach_project_id: params[:teach_project_id])
-    @evaluations = Evaluation.where(course: @courses)
-    @agregate = {
-      evaluations: @evaluations.count,
-      quality: average(:quality).to_f.round(2),
-      knowledge_acquired: nil,
-      technical_skills_acquired: 0.0,
-      soft_skills_acquired: 0.0,
-      relevant_content: [],
-      content_to_add: [],
-      content_to_remove: [],
-      comment: []
+    courses = Course.where(teach_project_id: params[:teach_project_id])
+    evaluations = Evaluation.where(course: courses)
+    render json: {
+      evaluations: evaluations.count,
+      quality: evaluations.average(:quality).to_f.round(2),
+      knowledge_acquired: evaluations.average(:knowledge_acquired).to_f.round(2),
+      technical_skills_acquired: evaluations.average(:technical_skills_acquired).to_f.round(2),
+      soft_skills_acquired: evaluations.average(:soft_skills_acquired).to_f.round(2),
+      relevant_content: evaluations.collect(&:relevant_content).reject {|e| e.blank?},
+      content_to_add: evaluations.collect(&:content_to_add).reject {|e| e.blank?},
+      content_to_remove: evaluations.collect(&:content_to_remove).reject {|e| e.blank?},
+      comment: evaluations.collect(&:comment).reject {|e| e.blank?}
     }
-    # @courses.each do |course|
-    #   @agregate[:evaluations] += course.evaluations.count
-    #   if @agregate[:quality].nil?
-    #     @agregate[:quality] = course.quality
-    #   else
-
-    #   end
-    # end
-    render json: @agregate
   end
 end
