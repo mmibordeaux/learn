01af006a51ef8044f8faf408a8dd59ac6c88e688
Arnaud Levy <contact@arnaudlevy.com>
2021-05-25 11:02:26 +0200

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,14 @@ Learn::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { host: 'learn.mmibordeaux.com' }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.sendgrid.net',
+  #   port: '587',
+  #   authentication: :plain,
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: 'heroku.com',
+  #   enable_starttls_auto: true
+  # }
 
 end
