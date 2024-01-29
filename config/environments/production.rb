c063a97ab46585f92ec8d5b47ef7124ede4c7f63
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-01-29 10:43:20 +0100

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,16 +67,8 @@ Learn::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { 
-    host: 'learn.mmibordeaux.com' 
-  }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: "smtp-relay.brevo.com",
-      port: 587,
-      user_name: ENV['SMTP_USER'],
-      password: ENV['SMTP_PASSWORD'],
-      authentication: :plain
+  config.action_mailer.default_url_options = {
+    host: 'learn.mmibordeaux.com'
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
